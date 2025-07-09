80b311e0efe69e6347afbd11d24aebbf7c4417d5
epickaowca <epickaowca@Epickas-Laptop.local>
2025-07-09 14:08:28 +0200

    form_request_refactor fix middleware

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
