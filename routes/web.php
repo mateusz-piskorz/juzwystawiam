63d76f73dd96911919c9e80d00c36465dd1ebf4b
epickaowca <epickaowca@Epickas-Laptop.local>
2025-08-14 20:21:39 +0200

    folder-structure-improvements fix routes

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,12 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('root/page');
+    return Inertia::render('root/page/index');
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
-        return Inertia::render('dashboard/page/index');
+        return Inertia::render('dashboard/page');
     })->name('dashboard');
 
     Route::resource('/dashboard/invoices', InvoiceController::class);
