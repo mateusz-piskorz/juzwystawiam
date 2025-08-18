5646f68c6bb9f1b46398b4585445677a8a25f021
epickaowca <epickaowca@Epickas-Laptop.local>
2025-08-18 08:52:01 +0200

    ziggy-fix

--- resources/js/lib/data/profile.ts
+++ resources/js/lib/data/profile.ts
@@ -2,21 +2,21 @@ import { apiFetch } from '@/lib/utils/api-fetch';
 import { DeleteAccountDTO, UpdatePasswordDTO, UpdateProfileDTO } from '../constants/zod/profile';
 
 export const updateProfile = async (body: UpdateProfileDTO) => {
-    return await apiFetch<{ success: true }>(route('api.profile.updateProfile'), {
+    return await apiFetch<{ success: true }>(route('api.profile.update-profile'), {
         method: 'PUT',
         body: JSON.stringify(body),
     });
 };
 
 export const updatePassword = async (body: UpdatePasswordDTO) => {
-    return await apiFetch<{ success: true }>(route('api.profile.updatePassword'), {
+    return await apiFetch<{ success: true }>(route('api.profile.update-password'), {
         method: 'PUT',
         body: JSON.stringify(body),
     });
 };
 
 export const deleteAccount = async (body: DeleteAccountDTO) => {
-    return await apiFetch<{ success: true }>(route('api.profile.deleteAccount'), {
+    return await apiFetch<{ success: true }>(route('api.profile.delete-account'), {
         method: 'DELETE',
         body: JSON.stringify(body),
     });
--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,36 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('ziggy', function () {
+
+    $exitCode = Artisan::call('ziggy:generate', [
+        '--types-only' => true
+    ]);
+
+    if ($exitCode === 0) {
+
+        $source = base_path('resources/js/ziggy.d.ts');
+        $destination = base_path('resources/js/lib/types/ziggy.d.ts');
+        if (file_exists($source)) {
+
+            $ziggyTypes = file_get_contents($source);
+
+            $ziggyTypes = preg_replace(
+                "/(declare module 'ziggy-js' {)/",
+                "$1\n    interface TypeConfig {\n        strictRouteNames: true;\n    }\n",
+                $ziggyTypes
+            );
+
+            file_put_contents($destination, $ziggyTypes);
+
+            unlink($source);
+
+            $this->info("resources/js/lib/types/ziggy.d.ts - generated successfully");
+        } else {
+            $this->error("ziggy.d.ts not found after generation.");
+        }
+    } else {
+        $this->error("ziggy:generate command failed.");
+    }
+});
