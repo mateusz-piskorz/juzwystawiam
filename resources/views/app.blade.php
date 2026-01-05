d59ef8135a1b80082afced09fa29b546f0bf904d
epickaowca <epickaowca@Epickas-Laptop.local>
2026-01-05 11:23:46 +0100

    favicon-imprv

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -34,13 +34,11 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-    <link rel="icon" href="/favicon.ico" sizes="any">
-    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <link rel="icon" href="/icon.png" sizes="166x114" type="image/png">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400..600&display=swap" rel="stylesheet" />
-    {{-- <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" /> --}}
+
 
     @routes
     @viteReactRefresh
