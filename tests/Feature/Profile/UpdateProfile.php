cfe7be481364fb8bd760d096d8422857045e8374
Mateusz Piskorz <54470703+mateusz-piskorz@users.noreply.github.com>
2026-01-21 16:28:54 +0100

    profile-api-tests (#45)
    Co-authored-by: epickaowca <epickaowca@Epickas-Laptop.local>

---
+++ tests/Feature/Profile/UpdateProfile.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\putJson;
+
+test('unauthenticated user cannot update profile', function () {
+    $user = User::factory()->create(['name' => 'Original Name']);
+    putJson(route('api.profile.update-profile'), ['name' => 'Updated Name'])->assertUnauthorized();
+    expect($user->fresh()->name)->toBe('Original Name');
+});
+
+test('user can update their profile', function () {
+    $user = User::factory()->create();
+
+    actingAs($user)
+        ->putJson(route('api.profile.update-profile'), ['name' => 'Updated Name'])
+        ->assertOk();
+
+    expect($user->fresh()->name)->toBe('Updated Name');
+});
